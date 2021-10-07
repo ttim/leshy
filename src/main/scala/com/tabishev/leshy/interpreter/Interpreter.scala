d5fcbd58e94c91a0561927c9c47553b403b15783
Timur Abishev <abishev.timur@gmail.com>
2021-10-07 18:41:27 -0400

    initial parser and interpreter

--- src/main/scala/com/tabishev/leshy/interpreter/Interpreter.scala
+++ src/main/scala/com/tabishev/leshy/interpreter/Interpreter.scala
@@ -1,16 +1,75 @@
 package com.tabishev.leshy.interpreter
 
+import com.tabishev.leshy.ast.{Const, Operation, Subroutine}
 import com.tabishev.leshy.loader.{FileLoader, RoutineLoader}
 
 import java.io.File
+import java.nio.ByteBuffer
 
 object Interpreter {
-  def run(loader: RoutineLoader, name: String): Unit = {
-    ???
-  }
+  def run(loader: RoutineLoader, name: String): Unit =
+    new InterpreterSession(loader).run(name)
 
   def main(args: Array[String]): Unit = {
     val loader = FileLoader.fromFile(new File("src/main/lsh/fib.lsh").toPath)
     run(loader, "main")
+  }
+}
+
+private class InterpreterSession(loader: RoutineLoader) {
+  var stack: Array[Byte] = Array.fill(10)(0)
+  var mirror: ByteBuffer = ByteBuffer.wrap(stack)
+  var size: Int = 0
+  var offset: Int = 0
+
+  def run(name: String): Unit = run(loader.load(name).get)
+
+  private def run(subroutine: Subroutine): Unit = {
+    subroutine.ops.foreach(run)
+  }
+
+  private def run(op: Operation): Unit = op match {
+    case Operation.Extend(length) => ???
+    case Operation.Append(bytes) => {
+      val evaled = evalConst(bytes)
+      extend(evaled.length)
+      System.arraycopy(evaled, 0, stack, size - evaled.length, evaled.length)
+    }
+    case Operation.Call(offsetConst, targetConst) => {
+      val offsetChange = evalConstAsInt(offsetConst)
+      offset += offsetChange
+      val target = evalConst(targetConst)
+      run(new String(target))
+      offset -= offsetChange
+    }
+    case Operation.CheckSize(length) => {
+      assert(evalConstAsInt(length) == size - offset)
+    }
+    case Operation.Branch(modifier, length, op1, op2, target) => {
+      val lengthEvaled = evalConstAsInt(length)
+      assert(lengthEvaled == 4)
+      ???
+    }
+  }
+
+  private def evalConst(const: Const): Array[Byte] = const match {
+    case Const.Literal(bytes) => bytes
+    case Const.Stack(from, length) => ???
+  }
+
+  private def evalConstAsInt(const: Const): Int = {
+    val bytes = evalConst(const)
+    assert(bytes.length == 4)
+    ByteBuffer.wrap(bytes).getInt
+  }
+
+  private def extend(extendSize: Int): Unit = {
+    if (size + extendSize <= stack.length) size += extendSize else {
+      val newStack = Array.fill[Byte](stack.length * 2)(0)
+      System.arraycopy(stack, 0, newStack, 0, stack.length)
+      stack = newStack
+      mirror = ByteBuffer.wrap(stack)
+      extend(extendSize)
+    }
   }
 }
--- src/main/scala/com/tabishev/leshy/parser/TextParser.scala
+++ src/main/scala/com/tabishev/leshy/parser/TextParser.scala
@@ -1,7 +1,88 @@
 package com.tabishev.leshy.parser
 
-import com.tabishev.leshy.ast.Subroutine
+import com.tabishev.leshy.ast.{Address, Const, Operation, Subroutine}
+
+import java.nio.ByteBuffer
+import java.nio.charset.Charset
 
 object TextParser {
-  def parse(text: String): Map[String, Subroutine] = ???
+  def parse(text: String): Map[String, Subroutine] =
+    ("\n" + text).split("\ndef ").tail.map(parseSubroutine).map { r => (r.name, r) }.toMap
+
+  private def parseSubroutine(text: String): Subroutine = {
+    val lines = text.split("\n")
+    Subroutine(lines.head, lines.toSeq.tail.flatMap(parseOperation))
+  }
+
+  private def parseOperation(text: String): Option[Operation] =
+    cleanOperation(text).split(" ").toSeq match {
+      // stack
+      case Seq("extend", length) =>
+        Some(Operation.Extend(parseConst(length)))
+      case Seq("shrink", length) =>
+        Some(Operation.Shrink(parseConst(length)))
+      case Seq("check_size", length) =>
+        Some(Operation.CheckSize(parseConst(length)))
+      case Seq("append", bytes) =>
+        Some(Operation.Append(parseConst(bytes)))
+
+      // branch
+      case Seq("branch", modifier, length, op1, op2, dest) =>
+        Some(Operation.Branch(parseConst(modifier), parseConst(length), parseConstOrAddress(op1), parseConstOrAddress(op2), parseConst(dest)))
+
+      // call
+      case Seq("call", offset, target) =>
+        Some(Operation.Call(parseConst(offset), parseConst(target)))
+
+      // memory
+      case Seq("copy", length, src, dst) =>
+        Some(Operation.Copy(parseConst(length), parseConstOrAddress(src), parseAddress(dst)))
+
+      // arithmetic
+      case Seq("add", length, op1, op2, dst) =>
+        Some(Operation.Add(parseConst(length), parseConstOrAddress(op1), parseConstOrAddress(op2), parseAddress(dst)))
+
+      // "syscalls"
+      case Seq("print_int", length, src) =>
+        Some(Operation.PrintInt(parseConst(length), parseAddress(src)))
+
+      case Seq("") =>
+        None
+      case _ =>
+        throw new IllegalArgumentException(s"can't parse operation '${cleanOperation(text)}'")
+    }
+
+  private def cleanOperation(operation: String): String = {
+    val noComment = if (operation.contains(";")) operation.substring(0, operation.indexOf(";")) else operation
+    noComment.trim
+  }
+
+  private def parseConst(arg: String): Const = {
+    if (arg.toIntOption.isDefined) {
+      val buffer = Array.fill[Byte](4)(0)
+      ByteBuffer.wrap(buffer).putInt(arg.toInt)
+      return Const.Literal(buffer)
+    }
+    if (arg.startsWith("'") && arg.endsWith("'")) {
+      // todo: remove this after I cover other cases
+      return Const.Literal(new String(arg.substring(1, arg.length - 1)).getBytes(Charset.forName("UTF-8")))
+    }
+    throw new IllegalArgumentException(s"can't parse const '$arg'")
+  }
+
+  private def parseAddress(arg: String): Address =
+    if (arg.startsWith("*#")) {
+      Address.IndirectHeap(parseConst(arg.substring(2)))
+    } else if (arg.startsWith("*")) {
+      Address.DirectHeap(parseConst(arg.substring(1)))
+    } else if (arg.startsWith("##")) {
+      ???
+    } else if (arg.startsWith("#")) {
+      Address.DirectStack(parseConst(arg.substring(1)))
+    } else {
+      throw new IllegalArgumentException(s"can't parse address '$arg'")
+    }
+
+  private def parseConstOrAddress(arg: String): Const | Address =
+    if (arg.startsWith("*") || arg.startsWith("#")) parseAddress(arg) else parseConst(arg)
 }
