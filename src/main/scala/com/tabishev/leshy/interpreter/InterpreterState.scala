6b3fc6a1c068c54dfee71638ef05a1580afb01e6
Timur Abishev <abishev.timur@gmail.com>
2021-10-09 01:12:23 -0400

    refactoring 1

--- src/main/scala/com/tabishev/leshy/interpreter/InterpreterState.scala
+++ src/main/scala/com/tabishev/leshy/interpreter/InterpreterState.scala
@@ -5,10 +5,24 @@ import java.nio.ByteBuffer
 // don't use byte buffers, and all these asInt etc. Just treat everything as byte arrays for real
 // create BytesRef type which is reference, and expose stackRef, heapRef, literalRef
 class InterpreterState {
-  private var stack: Array[Byte] = Array.fill(10)(0)
-  private var mirror: ByteBuffer = ByteBuffer.wrap(stack)
+  val stack = new StackMemory()
+
+  def getStack(length: Int, address: Int): Array[Byte] = {
+    val bytes = Array.fill[Byte](length)(0)
+    System.arraycopy(stack.stack, stack.offset + address, bytes, 0, length)
+    bytes
+  }
+
+  def putStack(index: Int, value: Array[Byte]): Unit = stack.mirror.put(stack.offset + index, value)
+  def putStackInt(index: Int, value: Int): Unit = stack.mirror.putInt(stack.offset + index, value)
+  def putStackLong(index: Int, value: Long): Unit = stack.mirror.putLong(stack.offset + index, value)
+}
+
+class StackMemory {
+  var stack: Array[Byte] = Array.fill(10)(0)
+  var mirror: ByteBuffer = ByteBuffer.wrap(stack)
   private var size: Int = 0
-  private var offset: Int = 0
+  var offset: Int = 0
 
   def extend(extendSize: Int): Unit = {
     if (size + extendSize <= stack.length) size += extendSize else {
@@ -31,18 +45,8 @@ class InterpreterState {
     System.arraycopy(bytes, 0, stack, size - bytes.length, bytes.length)
   }
 
-  def getStack(length: Int, address: Int): Array[Byte] = {
-    val bytes = Array.fill[Byte](length)(0)
-    System.arraycopy(stack, offset + address, bytes, 0, length)
-    bytes
-  }
-
-  def putStack(index: Int, value: Array[Byte]): Unit = mirror.put(offset + index, value)
-  def putStackInt(index: Int, value: Int): Unit = mirror.putInt(offset + index, value)
-  def putStackLong(index: Int, value: Long): Unit = mirror.putLong(offset + index, value)
+  def checkSize(size: Int): Unit = size == this.size - this.offset
 
   def advance(offset: Int): Unit = this.offset += offset
   def retreat(offset: Int): Unit = this.offset -= offset
-
-  def checkSize(size: Int): Unit = size == this.size - this.offset
 }
