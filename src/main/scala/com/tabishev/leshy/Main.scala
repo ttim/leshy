37d8f55bc3753dd3616f1107df8073b89751d790
Timur Abishev <abishev.timur@gmail.com>
2021-10-12 13:11:51 -0400

    move things to Main.scala and add main_big_factorial

---
+++ src/main/scala/com/tabishev/leshy/Main.scala
@@ -0,0 +1,25 @@
+package com.tabishev.leshy
+
+import com.tabishev.leshy.interpreter.Interpreter.run
+import com.tabishev.leshy.loader.FileLoader
+
+import java.io.File
+
+object Main {
+  def mainFib(): Unit = {
+    val loader = FileLoader.fromFile(new File("src/main/lsh/fib.lsh").toPath)
+    run(loader, "main", debug = false)
+  }
+
+  def mainFactorial(): Unit = {
+    val loader = FileLoader.fromFile(new File("src/main/lsh/factorial.lsh").toPath)
+    run(loader, "main", debug = false)
+  }
+
+  def mainFactorialLong(): Unit = {
+    val loader = FileLoader.fromFile(new File("src/main/lsh/factorial.lsh").toPath)
+    run(loader, "main_big_factorial", debug = false)
+  }
+
+  def main(args: Array[String]): Unit = mainFactorialLong()
+}
