87af7b7b54411efb167d2aef33784134abdf7b15
Timur Abishev <abishev.timur@gmail.com>
2021-10-13 12:09:47 -0400

    add tests on factorial too

--- src/main/scala/com/tabishev/leshy/bench/Bench.scala
+++ src/main/scala/com/tabishev/leshy/bench/Bench.scala
@@ -1,6 +1,7 @@
 package com.tabishev.leshy.bench
 
 import com.tabishev.leshy.ast.Bytes
+import com.tabishev.leshy.examples.Wrapper
 import com.tabishev.leshy.interpreter.{Interpreter, InterpreterState}
 import com.tabishev.leshy.loader.FileLoader
 import org.openjdk.jmh.annotations.*
@@ -12,10 +13,7 @@ import java.util.concurrent.TimeUnit
 object Bench {
   @State(Scope.Benchmark)
   class BenchState {
-    var interpreter: Interpreter = {
-      val loader = FileLoader.fromFile(new File("src/main/lsh/factorial.lsh").toPath)
-      new Interpreter(loader, debug = false)
-    }
+    var interpreter: Interpreter = Wrapper.baseInterpreter(debug = false)
   }
 }
 
@@ -27,17 +25,17 @@ object Bench {
 class Bench {
   @Benchmark
   def nativeFactorial4(bh: Blackhole): Unit =
-    bh.consume(Example.ffact4(10001))
+    bh.consume(Example.ffactorial4(10001))
 
   @Benchmark
   def nativeFactorial8(bh: Blackhole): Unit =
-    bh.consume(Example.ffact8(10001))
+    bh.consume(Example.ffactorial8(10001))
 
   @Benchmark
   def interpretFactorial4(bh: Blackhole, state: Bench.BenchState): Unit =
-    bh.consume(state.interpreter.run("test_ffact_4", Bytes.Empty))
+    bh.consume(Wrapper.ffactorial(state.interpreter, 4, 10001).asInt.get)
 
   @Benchmark
   def interpretFactorial8(bh: Blackhole, state: Bench.BenchState): Unit =
-    bh.consume(state.interpreter.run("test_ffact_8", Bytes.Empty))
+    bh.consume(Wrapper.ffactorial(state.interpreter, 8, 10001).asLong.get)
 }
