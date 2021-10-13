1424daae28d93a1a82b5bdf95712b36b0512eede
Timur Abishev <abishev.timur@gmail.com>
2021-10-13 14:43:00 -0400

    do not recreate buffer

--- src/main/scala/com/tabishev/leshy/examples/ByteBufferImpl.scala
+++ src/main/scala/com/tabishev/leshy/examples/ByteBufferImpl.scala
@@ -3,33 +3,35 @@ package com.tabishev.leshy.examples
 import java.nio.{ByteBuffer, ByteOrder}
 
 object ByteBufferImpl {
-  def ffactorial4(n: Int): Int = {
-    val bb = ByteBuffer.allocate(12)
+  private val buffer: ByteBuffer = {
+    val bb = ByteBuffer.allocate(24)
     bb.order(ByteOrder.LITTLE_ENDIAN)
-    bb.putInt(0, n) // n
-    bb.putInt(4, n) // i
-    bb.putInt(8, 1) // ans
+    bb
+  }
 
-    while (bb.getInt(4) > 0) {
-      bb.putInt(8, bb.getInt(8) * bb.getInt(4)) // ans = ans * i
-      bb.putInt(4, bb.getInt(4) - 2) // i = i - 2
+  def ffactorial4(n: Int): Int = {
+    buffer.putInt(0, n) // n
+    buffer.putInt(4, n) // i
+    buffer.putInt(8, 1) // ans
+
+    while (buffer.getInt(4) > 0) {
+      buffer.putInt(8, buffer.getInt(8) * buffer.getInt(4)) // ans = ans * i
+      buffer.putInt(4, buffer.getInt(4) - 2) // i = i - 2
     }
 
-    bb.getInt(8)
+    buffer.getInt(8)
   }
 
   def ffactorial8(n: Int): Long = {
-    val bb = ByteBuffer.allocate(24)
-    bb.order(ByteOrder.LITTLE_ENDIAN)
-    bb.putLong(0, n) // n
-    bb.putLong(8, n) // i
-    bb.putLong(16, 1) // ans
+    buffer.putLong(0, n) // n
+    buffer.putLong(8, n) // i
+    buffer.putLong(16, 1) // ans
 
-    while (bb.getLong(8) > 0) {
-      bb.putLong(16, bb.getLong(16) * bb.getLong(8)) // ans = ans * i
-      bb.putLong(8, bb.getLong(8) - 2) // i = i - 2
+    while (buffer.getLong(8) > 0) {
+      buffer.putLong(16, buffer.getLong(16) * buffer.getLong(8)) // ans = ans * i
+      buffer.putLong(8, buffer.getLong(8) - 2) // i = i - 2
     }
 
-    bb.getLong(16)
+    buffer.getLong(16)
   }
 }
