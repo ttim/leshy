fe6aa1e8bf58c850adea47498780a9dc5b3c0fe7
Timur Abishev <abishev.timur@gmail.com>
2021-10-13 16:58:36 -0400

    add bytes arrays based c impls

--- src/main/c/example.c
+++ src/main/c/example.c
@@ -1,3 +1,20 @@
+/**
+ffact4: 411587873, ffact8: 7031418319358416161
+ffact4_bytes: 411587873, ffact8_bytes: 7031418319358416161
+
+verification sum: 8231757460000
+ffact4 took 1.268600us
+
+verification sum: 8836313280411251232
+ffact8 took 6.148600us
+
+verification sum: 8231757460000
+ffact4_bytes took 0.538200us
+
+verification sum: 8836313280411251232
+ffact8_bytes took 6.157700us
+*/
+
 #include <stdio.h>
 #include <sys/time.h>
 
@@ -8,7 +25,7 @@ long current_usec() {
     return usec;
 }
 
-int ffact4(int n) {
+long ffact4(int n) {
     int ans = 1;
     int i = n;
     while (i > 0) {
@@ -17,6 +34,40 @@ int ffact4(int n) {
     }
 
     return ans;
+}
+
+void sum_int(int* arg1, int* arg2, int* dst) {
+    *dst = *arg1 + *arg2;
+}
+
+void mult_int(int* arg1, int* arg2, int* dst) {
+    *dst = *arg1 * *arg2;
+}
+
+void sum_long(long* arg1, long* arg2, long* dst) {
+    *dst = *arg1 + *arg2;
+}
+
+void mult_long(long* arg1, long* arg2, long* dst) {
+    *dst = *arg1 * *arg2;
+}
+
+long ffact4_bytes(int n) {
+    char bytes[12];
+    char* ref = &bytes;
+
+    int minus2 = -2;
+
+    *((int*) (ref + 8)) = 1;
+    *((int*) (ref + 4)) = n;
+
+    while (*((int*) (ref + 4)) > 0) {
+        mult_int(ref + 4, ref + 8, ref + 8);
+        sum_int(ref + 4, &minus2, ref + 4);
+        // printf("i, ans: %d, %d\n", *((int*) (ref + 4)), *((int*) (ref + 8)));
+    }
+
+    return *((int*) (ref + 8));
 }
 
 long ffact8(int n) {
@@ -30,26 +81,48 @@ long ffact8(int n) {
     return ans;
 }
 
+long ffact8_bytes(int n) {
+    char bytes[24];
+    char* ref = &bytes;
+
+    long minus2 = (long) -2;
+
+    *((long*) (ref + 16)) = (long) 1;
+    *((long*) (ref + 8)) = (long) n;
+
+    while (*((long*) (ref + 8)) > 0) {
+        mult_long(ref + 8, ref + 16, ref + 16);
+        sum_long(ref + 8, &minus2, ref + 8);
+        //printf("i, ans: %d, %d\n", *((long*) (ref + 8)), *((long*) (ref + 16)));
+    }
+
+    return *((long*) (ref + 16));
+}
+
 void test(int n, char* name, void* fn(int)) {
     int repeat = 10000;
-    int sum = 0;
+    long sum = 0;
 
     // warmup
-    for (int i = 0; i < repeat; i++) sum += (int) fn(n);
+    for (int i = 0; i < repeat; i++) sum += (long) fn(n);
 
     // main
     long beginning = current_usec();
-    for (int i = 0; i < repeat; i++) sum += (int) fn(n);
+    for (int i = 0; i < repeat; i++) sum += (long) fn(n);
     long elapsed = current_usec() - beginning;
 
-    printf("verification sum: %d\n", sum);
+    printf("verification sum: %ld\n", sum);
     printf(name);
-    printf(" took %fus\n", elapsed * 1.0 / repeat);
+    printf(" took %fus\n\n", elapsed * 1.0 / repeat);
 }
 
 int main() {
     printf("ffact4: %d, ffact8: %ld\n", ffact4(10001), ffact8(10001));
+    printf("ffact4_bytes: %d, ffact8_bytes: %ld\n", ffact4_bytes(10001), ffact8_bytes(10001));
+    printf("\n");
 
     test(10001, "ffact4", *ffact4);
     test(10001, "ffact8", *ffact8);
+    test(10001, "ffact4_bytes", *ffact4_bytes);
+    test(10001, "ffact8_bytes", *ffact8_bytes);
 }
