9e61a097acf0e9fa09a1c4ec76b93cec59fb41a2
Timur Abishev <abishev.timur@gmail.com>
2021-10-13 12:11:10 -0400

    rename

--- src/main/scala/com/tabishev/leshy/bench/Bench.scala
+++ src/main/scala/com/tabishev/leshy/bench/Bench.scala
@@ -1,7 +1,7 @@
 package com.tabishev.leshy.bench
 
 import com.tabishev.leshy.ast.Bytes
-import com.tabishev.leshy.examples.Wrapper
+import com.tabishev.leshy.examples.{JavaImpl, LshImpl}
 import com.tabishev.leshy.interpreter.{Interpreter, InterpreterState}
 import com.tabishev.leshy.loader.FileLoader
 import org.openjdk.jmh.annotations.*
@@ -13,7 +13,7 @@ import java.util.concurrent.TimeUnit
 object Bench {
   @State(Scope.Benchmark)
   class BenchState {
-    var interpreter: Interpreter = Wrapper.baseInterpreter(debug = false)
+    var interpreter: Interpreter = LshImpl.baseInterpreter(debug = false)
   }
 }
 
@@ -25,17 +25,17 @@ object Bench {
 class Bench {
   @Benchmark
   def nativeFactorial4(bh: Blackhole): Unit =
-    bh.consume(Example.ffactorial4(10001))
+    bh.consume(JavaImpl.ffactorial4(10001))
 
   @Benchmark
   def nativeFactorial8(bh: Blackhole): Unit =
-    bh.consume(Example.ffactorial8(10001))
+    bh.consume(JavaImpl.ffactorial8(10001))
 
   @Benchmark
   def interpretFactorial4(bh: Blackhole, state: Bench.BenchState): Unit =
-    bh.consume(Wrapper.ffactorial(state.interpreter, 4, 10001).asInt.get)
+    bh.consume(LshImpl.ffactorial(state.interpreter, 4, 10001).asInt.get)
 
   @Benchmark
   def interpretFactorial8(bh: Blackhole, state: Bench.BenchState): Unit =
-    bh.consume(Wrapper.ffactorial(state.interpreter, 8, 10001).asLong.get)
+    bh.consume(LshImpl.ffactorial(state.interpreter, 8, 10001).asLong.get)
 }
--- src/test/scala/com/tabishev/leshy/InterpreterSpec.scala
+++ src/test/scala/com/tabishev/leshy/InterpreterSpec.scala
@@ -1,55 +1,54 @@
 package com.tabishev.leshy
 
 import com.tabishev.leshy.ast.Bytes
-import com.tabishev.leshy.bench.Example
-import com.tabishev.leshy.examples.Wrapper
+import com.tabishev.leshy.examples.{JavaImpl, LshImpl}
 import com.tabishev.leshy.interpreter.Interpreter
 import com.tabishev.leshy.loader.FileLoader
 
 import java.io.File
 
 class InterpreterSpec extends munit.FunSuite {
-  val interpreter = Wrapper.baseInterpreter(false)
+  val interpreter = LshImpl.baseInterpreter(false)
 
   test("fib4 works") {
     testFib(
-      n => Wrapper.fib4(interpreter, n),
-      n => Example.fib4(n)
+      n => LshImpl.fib4(interpreter, n),
+      n => JavaImpl.fib4(n)
     )
   }
 
   test("fib8 works") {
     testFib(
-      n => Wrapper.fib8(interpreter, n),
-      n => Example.fib8(n)
+      n => LshImpl.fib8(interpreter, n),
+      n => JavaImpl.fib8(n)
     )
   }
 
   test("fibx4 works") {
     testFib(
-      n => Wrapper.fibx4(interpreter, n),
-      n => Example.fib4(n)
+      n => LshImpl.fibx4(interpreter, n),
+      n => JavaImpl.fib4(n)
     )
   }
 
   test("fibx8 works") {
     testFib(
-      n => Wrapper.fibx8(interpreter, n),
-      n => Example.fib8(n)
+      n => LshImpl.fibx8(interpreter, n),
+      n => JavaImpl.fib8(n)
     )
   }
 
   test("ffactorial 4 works") {
     testFfactorial(
-      n => Wrapper.ffactorial(interpreter, 4, n).asInt.get,
-      n => Example.ffactorial4(n)
+      n => LshImpl.ffactorial(interpreter, 4, n).asInt.get,
+      n => JavaImpl.ffactorial4(n)
     )
   }
 
   test("ffactorial 8 works") {
     testFfactorial(
-      n => Wrapper.ffactorial(interpreter, 8, n).asLong.get,
-      n => Example.ffactorial8(n)
+      n => LshImpl.ffactorial(interpreter, 8, n).asLong.get,
+      n => JavaImpl.ffactorial8(n)
     )
   }
 
