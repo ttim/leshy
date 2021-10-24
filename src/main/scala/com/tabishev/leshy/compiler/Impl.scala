c767846fbbdef24cf75e54a8f200de2e37bcb9b2
Timur Abishev <abishev.timur@gmail.com>
2021-10-24 11:28:44 -0400

    more some impl into Impl

--- src/main/scala/com/tabishev/leshy/compiler/Impl.scala
+++ src/main/scala/com/tabishev/leshy/compiler/Impl.scala
@@ -68,6 +68,22 @@ object Sum {
     override def execute(runtime: Runtime): Unit =
       dst.materialize(runtime).putLong(op1.materialize(runtime).getLong() + op2)
   }
+
+  def length4(op1Union: MemoryOperand | Int, op2Union: MemoryOperand | Int, dst: MemoryOperand): SimpleImpl =
+    (op1Union, op2Union) match {
+      case (op1: MemoryOperand, op2: MemoryOperand) => MM4(op1, op2, dst)
+      case (op1: MemoryOperand, op2: Int) => MC4(op1, op2, dst)
+      case (op1: Int, op2: MemoryOperand) => MC4(op2, op1, dst)
+      case (op1: Int, op2: Int) => Const.Write4(op1 + op2, dst)
+    }
+
+  def length8(op1Union: MemoryOperand | Long, op2Union: MemoryOperand | Long, dst: MemoryOperand): SimpleImpl =
+    (op1Union, op2Union) match {
+      case (op1: MemoryOperand, op2: MemoryOperand) => MM8(op1, op2, dst)
+      case (op1: MemoryOperand, op2: Long) => MC8(op1, op2, dst)
+      case (op1: Long, op2: MemoryOperand) => MC8(op2, op1, dst)
+      case (op1: Long, op2: Long) => Const.Write8(op1 + op2, dst)
+    }
 }
 
 object Mult {
@@ -91,6 +107,22 @@ object Mult {
     override def execute(runtime: Runtime): Unit =
       dst.materialize(runtime).putLong(op1.materialize(runtime).getLong() * op2)
   }
+
+  def length4(op1Union: MemoryOperand | Int, op2Union: MemoryOperand | Int, dst: MemoryOperand): SimpleImpl =
+    (op1Union, op2Union) match {
+      case (op1: MemoryOperand, op2: MemoryOperand) => MM4(op1, op2, dst)
+      case (op1: MemoryOperand, op2: Int) => MC4(op1, op2, dst)
+      case (op1: Int, op2: MemoryOperand) => MC4(op2, op1, dst)
+      case (op1: Int, op2: Int) => Const.Write4(op1 * op2, dst)
+    }
+
+  def length8(op1Union: MemoryOperand | Long, op2Union: MemoryOperand | Long, dst: MemoryOperand): SimpleImpl =
+    (op1Union, op2Union) match {
+      case (op1: MemoryOperand, op2: MemoryOperand) => MM8(op1, op2, dst)
+      case (op1: MemoryOperand, op2: Long) => MC8(op1, op2, dst)
+      case (op1: Long, op2: MemoryOperand) => MC8(op2, op1, dst)
+      case (op1: Long, op2: Long) => Const.Write8(op1 * op2, dst)
+    }
 }
 
 object Negate {
@@ -102,6 +134,16 @@ object Negate {
   case class M8(op: MemoryOperand, dst: MemoryOperand) extends SimpleImpl with Length8 {
     override def execute(runtime: Runtime): Unit =
       dst.materialize(runtime).putLong(op.materialize(runtime).getLong())
+  }
+
+  def length4(opUnion: MemoryOperand | Int, dst: MemoryOperand): SimpleImpl = opUnion match {
+    case op: MemoryOperand => M4(op, dst)
+    case op: Int => Const.Write4(-op, dst)
+  }
+
+  def length8(opUnion: MemoryOperand | Long, dst: MemoryOperand): SimpleImpl = opUnion match {
+    case op: MemoryOperand => M8(op, dst)
+    case op: Long => Const.Write8(-op, dst)
   }
 }
 
@@ -115,6 +157,16 @@ object Set {
     override def execute(runtime: Runtime): Unit =
       dst.materialize(runtime).putLong(src.materialize(runtime).getLong())
   }
+
+  def length4(srcUnion: MemoryOperand | Int, dst: MemoryOperand): SimpleImpl = srcUnion match {
+    case src: MemoryOperand => M4(src, dst)
+    case src: Int => Const.Write4(src, dst)
+  }
+
+  def length8(srcUnion: MemoryOperand | Long, dst: MemoryOperand): SimpleImpl = srcUnion match {
+    case src: MemoryOperand => M8(src, dst)
+    case src: Long => Const.Write8(src, dst)
+  }
 }
 
 object Branch {
@@ -125,6 +177,8 @@ object Branch {
   case object Never extends BranchImpl {
     override def execute(runtime: Runtime): Boolean = false
   }
+
+  def fromFlag(flag: Boolean): BranchImpl = if (flag) Always else Never
 
   case class MoreMM4(op1: MemoryOperand, op2: MemoryOperand) extends BranchImpl {
     override def execute(runtime: Runtime): Boolean =
@@ -161,4 +215,36 @@ object Branch {
   case class LessOrEqualMC8(op1: MemoryOperand, op2: Long) extends BranchImpl {
     override def execute(runtime: Runtime): Boolean = op1.materialize(runtime).getLong() <= op2
   }
+
+  def more4(op1Union: MemoryOperand | Int, op2Union: MemoryOperand | Int): BranchImpl =
+    (op1Union, op2Union) match {
+      case (op1: MemoryOperand, op2: MemoryOperand) => MoreMM4(op1, op2)
+      case (op1: MemoryOperand, op2: Int) => MoreMC4(op1, op2)
+      case (op1: Int, op2: MemoryOperand) => LessOrEqualMC4(op2, op1)
+      case (op1: Int, op2: Int) => fromFlag(op1 > op2)
+    }
+
+  def more8(op1Union: MemoryOperand | Long, op2Union: MemoryOperand | Long): BranchImpl =
+    (op1Union, op2Union) match {
+      case (op1: MemoryOperand, op2: MemoryOperand) => MoreMM8(op1, op2)
+      case (op1: MemoryOperand, op2: Long) => MoreMC8(op1, op2)
+      case (op1: Long, op2: MemoryOperand) => LessOrEqualMC8(op2, op1)
+      case (op1: Long, op2: Long) => fromFlag(op1 > op2)
+    }
+
+  def lessOrEqual4(op1Union: MemoryOperand | Int, op2Union: MemoryOperand | Int): BranchImpl =
+    (op1Union, op2Union) match {
+      case (op1: MemoryOperand, op2: MemoryOperand) => MoreMM4(op2, op1)
+      case (op1: MemoryOperand, op2: Int) => LessOrEqualMC4(op1, op2)
+      case (op1: Int, op2: MemoryOperand) => MoreMC4(op2, op1)
+      case (op1: Int, op2: Int) => fromFlag(op1 <= op2)
+    }
+
+  def lessOrEqual8(op1Union: MemoryOperand | Long, op2Union: MemoryOperand | Long): BranchImpl =
+    (op1Union, op2Union) match {
+      case (op1: MemoryOperand, op2: MemoryOperand) => MoreMM8(op2, op1)
+      case (op1: MemoryOperand, op2: Long) => LessOrEqualMC8(op1, op2)
+      case (op1: Long, op2: MemoryOperand) => MoreMC8(op2, op1)
+      case (op1: Long, op2: Long) => fromFlag(op1 <= op2)
+    }
 }
