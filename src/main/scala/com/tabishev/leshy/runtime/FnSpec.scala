fe7cf340192c4275b452887d1fd859d237aea01d
Timur Abishev <abishev.timur@gmail.com>
2021-11-02 23:02:51 -0600

    rework passing consts in spec

--- src/main/scala/com/tabishev/leshy/runtime/FnSpec.scala
+++ src/main/scala/com/tabishev/leshy/runtime/FnSpec.scala
@@ -2,10 +2,24 @@ package com.tabishev.leshy.runtime
 
 import com.tabishev.leshy.ast.Bytes
 
-case class FnSpec[T, V](fn: String, input: (T, Runtime) => Unit, output: Bytes => V) {
+case class FnSpec[T, V](fn: String, input: T => Input, output: Bytes => V) {
   def map[V2](f: V => V2): FnSpec[T, V2] = FnSpec(fn, input, output.andThen(f))
 
-  def contramap[T2](f: T2 => T): FnSpec[T2, V] = FnSpec(fn, {
-    (arg, memory) => input(f(arg), memory)
-  }, output)
+  def contramap[T2](f: T2 => T): FnSpec[T2, V] = FnSpec(fn, f.andThen(input), output)
+}
+
+object Input {
+  val Empty: Input = Input(Bytes.Empty, Consts.Empty)
+
+  def add(bytes: Bytes, isConst: Boolean): Input = Input.Empty.add(bytes, isConst)
+}
+
+case class Input private (bytes: Bytes, consts: Consts) {
+  def add(toAdd: Bytes, isConst: Boolean): Input = {
+    val newConsts = if (isConst)
+      consts.markConsts(FrameOffset.nonNegative(bytes.length()), toAdd.get())
+    else
+      consts
+    Input(Bytes.fromBytes(bytes.get() ++ toAdd.get()), newConsts)
+  }
 }
