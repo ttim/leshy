7f4f8a9ccac554faa915621b2334a7a29d741b72
Timur Abishev <abishev.timur@gmail.com>
2021-11-03 09:52:06 -0600

    add no-check to bench

--- src/main/scala/com/tabishev/leshy/bench/FfactorialBench.scala
+++ src/main/scala/com/tabishev/leshy/bench/FfactorialBench.scala
@@ -14,7 +14,7 @@ import java.util.concurrent.TimeUnit
 @Measurement(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
 @State(Scope.Benchmark)
 class FfactorialBench {
-  @Param(Array("interpreter/generic", "compiler/generic", "native", "byte buffer", "memory access"))
+  @Param(Array("interpreter/generic", "interpreter/generic/no-check", "compiler/generic", "native", "byte buffer", "memory access"))
   var impl: String = _
 
   @Benchmark
