2e82777c6ca488bef8186e366ea3ba5002f6e796
Timur Abishev <abishev.timur@gmail.com>
2021-11-12 02:25:58 -0500

    no more generic nodes

--- src/main/scala/com/tabishev/leshy/compiler/NodeFactory.scala
+++ src/main/scala/com/tabishev/leshy/compiler/NodeFactory.scala
@@ -15,8 +15,6 @@ object NodeFactory {
     val options = Node.Options(compiler.debugEnabled, op, ctx)
     val constInterpreter = SpecializationContextConstInterpreter(symbols, ctx)
 
-    def node(op: OperationRef): GenericNode = GenericNode.of(ctx => compiler.create(op, ctx))
-
     def executeNode(execution: Execution): Node = {
       val (stackSize, prevConsts) = ctx.get()
       val nextCtx = SpecializationContext.from(execution.stackSize(stackSize), execution.markConsts(prevConsts))
@@ -28,11 +26,15 @@ object NodeFactory {
       val targetOptions = Node.Options(compiler.debugEnabled, target, ctx)
       Node.LazyNode(targetOptions, () => compiler.create(target, ctx))
     }
-    def callNode(fn: String, offset: FrameOffset): Node = {
+    def fnCallNode(fn: String, offset: FrameOffset): Node = {
       val target = OperationRef(fn, 0)
-      val callCtx = SpecializationContext.offset(options.ctx, offset)
+      val callCtx = SpecializationContext.offset(ctx, offset)
       val callOptions = Node.Options(compiler.debugEnabled, target, callCtx)
       Node.LazyNode(callOptions, () => compiler.create(target, callCtx))
+    }
+    def fnCallNextNode(calleeCtx: SpecializationContext, offset: FrameOffset): Node = {
+      val nextCtx = SpecializationContext.fnCall(ctx, offset, calleeCtx)
+      compiler.create(op.next, nextCtx)
     }
 
     def toOperand(address: ast.Address): MemoryOperand = toOperandFn(constInterpreter, address)
@@ -51,7 +53,7 @@ object NodeFactory {
         case ast.Operation.Call(offsetAst, targetAst) =>
           val offset = constInterpreter.evalOffset(offsetAst)
           val target = constInterpreter.evalSymbol(targetAst).name
-          Node.Call(options, offset, callNode(target, offset), node(op.next))
+          Node.Call(options, offset, fnCallNode(target, offset), calleeCtx => fnCallNextNode(calleeCtx, offset))
         case ast.Operation.CheckSize(lengthAst) =>
           assert(constInterpreter.evalLength(lengthAst) == constInterpreter.frameSize())
           create(compiler, symbols, ctx, op.next, fn)
