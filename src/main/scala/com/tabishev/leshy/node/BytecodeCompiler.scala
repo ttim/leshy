de6155f29179ae04dc41c26df8aae38e5a4586f9
Timur Abishev <abishev.timur@gmail.com>
2021-11-13 18:26:49 -0500

    minor renames

--- src/main/scala/com/tabishev/leshy/node/BytecodeCompiler.scala
+++ src/main/scala/com/tabishev/leshy/node/BytecodeCompiler.scala
@@ -2,7 +2,7 @@ package com.tabishev.leshy.node
 
 import com.tabishev.leshy.runtime.Runtime
 
-object Compiler {
+object BytecodeCompiler {
   def compile(node: Node): Node.Generated =
     new Node.Generated {
       override def runInternal(runtime: Runtime): Node = node.runInternal(runtime)
