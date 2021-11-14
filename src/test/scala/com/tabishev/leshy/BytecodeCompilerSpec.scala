57c393d7a3e0ca4bf08f80967a91bc9f2441af81
Timur Abishev <abishev.timur@gmail.com>
2021-11-14 17:43:52 -0500

    minor

--- src/test/scala/com/tabishev/leshy/BytecodeCompilerSpec.scala
+++ src/test/scala/com/tabishev/leshy/BytecodeCompilerSpec.scala
@@ -4,44 +4,48 @@ import com.tabishev.leshy
 import com.tabishev.leshy.compiler.{BranchExecution, Const, Execution, MemoryOperand, Nodes, OperationRef, Stack}
 import com.tabishev.leshy.examples.Implementations
 import com.tabishev.leshy.node.{BytecodeCompiler, Node}
-import com.tabishev.leshy.runtime.{FrameOffset, Runtime}
+import com.tabishev.leshy.runtime.{FrameOffset, Runtime, StackMemory}
 
 import java.io.File
 import java.util
 import scala.util.Random
 
 class BytecodeCompilerSpec extends munit.FunSuite {
+  private val prepareNothing: StackMemory => Unit = { _ => () }
+
+  private def prepareSize(size: Int): StackMemory => Unit = { stack => stack.setFramesize(size) }
+
   private def genOrigin(): Nodes.Origin = Nodes.Origin(null, OperationRef("", Random.nextInt()), null)
   private def finalNode(): Node = Nodes.Final(genOrigin())
-  private def executeNode(size: Int, ex: Execution): Node = Nodes.Execute(genOrigin(), Nodes.Execute(genOrigin(), finalNode(), ex), Stack.SetSize(0, size))
+  private def executeNode(ex: Execution): Node = Nodes.Execute(genOrigin(), finalNode(), ex)
 
   test("write4") {
-    testExecution(4, Const.Write4(777, MemoryOperand.Stack(FrameOffset.Zero)))
+    testExecution(prepareSize(4), Const.Write4(777, MemoryOperand.Stack(FrameOffset.Zero)))
   }
 
   test("write8") {
-    testExecution(8, Const.Write8(Long.MaxValue - 777, MemoryOperand.Stack(FrameOffset.Zero)))
+    testExecution(prepareSize(4), Const.Write8(Long.MaxValue - 777, MemoryOperand.Stack(FrameOffset.Zero)))
   }
 
   test("setSize") {
-    testExecution(0, Stack.SetSize(0, 12))
+    testExecution(prepareNothing, Stack.SetSize(0, 12))
   }
 
   test("testFlagBranch") {
-    testBranch(BranchExecution.Const(true))
-    testBranch(BranchExecution.Const(false))
+    testBranch(prepareSize(4), BranchExecution.Const(true))
+    testBranch(prepareSize(4), BranchExecution.Const(false))
   }
 
-  private def testExecution(size: Int, ex: Execution): Unit = check(executeNode(size, ex))
+  private def testExecution(prepare: StackMemory => Unit, ex: Execution): Unit = check(prepare, executeNode(ex))
 
-  private def testBranch(ex: BranchExecution): Unit = check(Nodes.Branch(
+  private def testBranch(prepare: StackMemory => Unit, ex: BranchExecution): Unit = check(prepare, Nodes.Branch(
     genOrigin(),
-    executeNode(4, Const.Write4(777, MemoryOperand.Stack(FrameOffset.Zero))),
-    executeNode(4, Const.Write4(888, MemoryOperand.Stack(FrameOffset.Zero))),
+    executeNode(Const.Write4(777, MemoryOperand.Stack(FrameOffset.Zero))),
+    executeNode(Const.Write4(888, MemoryOperand.Stack(FrameOffset.Zero))),
     ex
   ))
 
-  private def check(node: Node): Unit = {
+  private def check(prepare: StackMemory => Unit, node: Node): Unit = {
     val expectedRuntime = new Runtime()
     val expectedFinal = node.run(expectedRuntime)
     val expected = expectedRuntime.stack.currentStackFrame()
