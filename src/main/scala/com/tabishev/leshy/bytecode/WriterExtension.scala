b21ce833b8ae1c9ada58ba7ca0c0bf36d490b85a
Timur Abishev <abishev.timur@gmail.com>
2021-11-15 01:38:27 -0500

    move kind to push

--- src/main/scala/com/tabishev/leshy/bytecode/WriterExtension.scala
+++ src/main/scala/com/tabishev/leshy/bytecode/WriterExtension.scala
@@ -4,13 +4,13 @@ import org.objectweb.asm.{MethodVisitor, Opcodes, Type}
 
 extension (writer: MethodVisitor) {
   def statement[T](value: T)(using pushable: BytecodeExpression[T]): Unit = {
-    push(value)
-    pushable.kind(value).popInst.foreach(writer.visitInsn(_))
+    val kind = push(value)
+    kind.popInst.foreach(writer.visitInsn(_))
   }
 
   def ret[T](value: T)(using pushable: BytecodeExpression[T]): Unit = {
-    push(value)
-    writer.visitInsn(pushable.kind(value).retInst)
+    val kind = push(value)
+    writer.visitInsn(kind.retInst)
   }
 
   def putField[T](field: Field, value: T)(using pushable: BytecodeExpression[T]): Unit =
@@ -22,5 +22,5 @@ extension (writer: MethodVisitor) {
       writer.visitFieldInsn(Opcodes.PUTFIELD, field.owner.getInternalName, field.name, field.tpe.getDescriptor)
     }
 
-  def push[T](value: T)(using pushable: BytecodeExpression[T]): Unit = pushable.push(writer, value)
+  def push[T](value: T)(using pushable: BytecodeExpression[T]): BytecodeExpression.Kind = pushable.push(writer, value)
 }
