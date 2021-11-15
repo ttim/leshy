aee786e51ffde625ee539fc30ede0f644a6580f8
Timur Abishev <abishev.timur@gmail.com>
2021-11-15 01:05:44 -0500

    rename pushable to expression

--- src/main/scala/com/tabishev/leshy/bytecode/BytecodeExpression.scala
+++ src/main/scala/com/tabishev/leshy/bytecode/BytecodeExpression.scala
@@ -5,13 +5,13 @@ import org.objectweb.asm.{MethodVisitor, Opcodes, Type}
 import java.lang.reflect.Method
 import scala.reflect.ClassTag
 
-trait Pushable[T] {
+trait BytecodeExpression[T] {
   def push(writer: MethodVisitor, value: T): Unit
 
-  def kind(value: T): Pushable.Kind
+  def kind(value: T): BytecodeExpression.Kind
 }
 
-object Pushable {
+object BytecodeExpression {
   enum Kind {
     case Void
     case Object
@@ -49,27 +49,27 @@ object Pushable {
     case Type.LONG => Kind.Long
   }
 
-  def from[TIn, TOut: Pushable](fn: TIn => TOut): Pushable[TIn] = new Pushable[TIn] {
+  def from[TIn, TOut: BytecodeExpression](fn: TIn => TOut): BytecodeExpression[TIn] = new BytecodeExpression[TIn] {
     override def push(writer: MethodVisitor, value: TIn): Unit = writer.push(fn(value))
-    override def kind(value: TIn): Kind = implicitly[Pushable[TOut]].kind(fn(value))
+    override def kind(value: TIn): Kind = implicitly[BytecodeExpression[TOut]].kind(fn(value))
   }
 
-  def from[TIn, TOut: Pushable](other: TOut): Pushable[TIn] = from(_ => other)
+  def from[TIn, TOut: BytecodeExpression](other: TOut): BytecodeExpression[TIn] = from(_ => other)
 }
 
-given intPushable: Pushable[Int] with {
+given intBytecodeExpression: BytecodeExpression[Int] with {
   override def push(writer: MethodVisitor, value: Int): Unit = writer.visitLdcInsn(value)
-  override def kind(value: Int): Pushable.Kind = Pushable.Kind.Int
+  override def kind(value: Int): BytecodeExpression.Kind = BytecodeExpression.Kind.Int
 }
 
-given longPushable: Pushable[Long] with {
+given longBytecodeExpression: BytecodeExpression[Long] with {
   override def push(writer: MethodVisitor, value: Long): Unit = writer.visitLdcInsn(value)
-  override def kind(value: Long): Pushable.Kind = Pushable.Kind.Long
+  override def kind(value: Long): BytecodeExpression.Kind = BytecodeExpression.Kind.Long
 }
 
-given booleanPushable: Pushable[Boolean] with {
+given booleanBytecodeExpression: BytecodeExpression[Boolean] with {
   override def push(writer: MethodVisitor, value: Boolean): Unit = writer.visitLdcInsn(if (value) 1 else 0)
-  override def kind(value: Boolean): Pushable.Kind = Pushable.Kind.Int
+  override def kind(value: Boolean): BytecodeExpression.Kind = BytecodeExpression.Kind.Int
 }
 
 
@@ -84,41 +84,41 @@ object InvokeMethod {
   def staticValues(clazz: Class[_], name: String, args: Value[_]*): InvokeMethod =
     InvokeMethod(Opcodes.INVOKESTATIC, clazz, name, args.toSeq)
 
-  def virtual[T1: Pushable](clazz: Class[_], name: String, arg1: T1): InvokeMethod =
+  def virtual[T1: BytecodeExpression](clazz: Class[_], name: String, arg1: T1): InvokeMethod =
     virtualValues(clazz, name, Value(arg1))
 
-  def virtual[T1: Pushable, T2: Pushable](clazz: Class[_], name: String, arg1: T1, arg2: T2): InvokeMethod =
+  def virtual[T1: BytecodeExpression, T2: BytecodeExpression](clazz: Class[_], name: String, arg1: T1, arg2: T2): InvokeMethod =
     virtualValues(clazz, name, Value(arg1), Value(arg2))
 
-  def static[T1: Pushable](clazz: Class[_], name: String, arg1: T1): InvokeMethod =
+  def static[T1: BytecodeExpression](clazz: Class[_], name: String, arg1: T1): InvokeMethod =
     staticValues(clazz, name, Value(arg1))
 }
 
-case class Param(idx: Int, kind: Pushable.Kind)
+case class Param(idx: Int, kind: BytecodeExpression.Kind)
 
 object Param {
-  def idx[T: ClassTag](idx: Int): Param = Param(idx, Pushable.kind[T])
+  def idx[T: ClassTag](idx: Int): Param = Param(idx, BytecodeExpression.kind[T])
 }
 
 case class ThisInstance()
 
 case class InvokeSuper(superClass: Class[_])
 
-given superPushable: Pushable[InvokeSuper] with {
+given superBytecodeExpression: BytecodeExpression[InvokeSuper] with {
   override def push(writer: MethodVisitor, value: InvokeSuper): Unit = {
     writer.visitVarInsn(Opcodes.ALOAD, 0)
     writer.visitMethodInsn(Opcodes.INVOKESPECIAL, Type.getInternalName(value.superClass), "<init>", "()V", false)
   }
 
-  override def kind(value: InvokeSuper): Pushable.Kind = Pushable.Kind.Void
+  override def kind(value: InvokeSuper): BytecodeExpression.Kind = BytecodeExpression.Kind.Void
 }
 
-given thisPushable: Pushable[ThisInstance] with {
+given thisBytecodeExpression: BytecodeExpression[ThisInstance] with {
   override def push(writer: MethodVisitor, value: ThisInstance): Unit = writer.visitVarInsn(Opcodes.ALOAD, 0)
-  override def kind(value: ThisInstance): Pushable.Kind = Pushable.Kind.Object
+  override def kind(value: ThisInstance): BytecodeExpression.Kind = BytecodeExpression.Kind.Object
 }
 
-given fieldPushable: Pushable[Field] with {
+given fieldBytecodeExpression: BytecodeExpression[Field] with {
   override def push(writer: MethodVisitor, value: Field): Unit = {
     if (!value.isStatic) {
       writer.push(ThisInstance())
@@ -127,17 +127,17 @@ given fieldPushable: Pushable[Field] with {
       ???
     }
   }
-  override def kind(value: Field): Pushable.Kind = Pushable.kind(value.tpe)
+  override def kind(value: Field): BytecodeExpression.Kind = BytecodeExpression.kind(value.tpe)
 }
 
-given invokeMethodPushable: Pushable[InvokeMethod] with {
+given invokeMethodBytecodeExpression: BytecodeExpression[InvokeMethod] with {
   override def push(writer: MethodVisitor, value: InvokeMethod): Unit = {
     value.args.foreach(_.push(writer))
-    assert(value.args.forall(_.kind != Pushable.Kind.Void))
+    assert(value.args.forall(_.kind != BytecodeExpression.Kind.Void))
     writer.visitMethodInsn(value.opcode, Type.getType(value.clazz).getInternalName, value.name, Type.getMethodDescriptor(method(value)), false)
   }
 
-  override def kind(value: InvokeMethod): Pushable.Kind = Pushable.kind(Type.getType(method(value).getReturnType))
+  override def kind(value: InvokeMethod): BytecodeExpression.Kind = BytecodeExpression.kind(Type.getType(method(value).getReturnType))
 
   private def method(value: InvokeMethod): Method = {
     val methods = value.clazz.getMethods.filter(_.getName == value.name)
@@ -146,16 +146,16 @@ given invokeMethodPushable: Pushable[InvokeMethod] with {
   }
 }
 
-given paramPushable: Pushable[Param] with {
+given paramBytecodeExpression: BytecodeExpression[Param] with {
   override def push(writer: MethodVisitor, value: Param): Unit =
     value.kind.loadInst.foreach { inst =>
       writer.visitVarInsn(inst, value.idx + 1)
     }
 
-  override def kind(value: Param): Pushable.Kind = value.kind
+  override def kind(value: Param): BytecodeExpression.Kind = value.kind
 }
 
-case class Value[T: Pushable](value: T) {
-  def push(writer: MethodVisitor): Unit = implicitly[Pushable[T]].push(writer, value)
-  def kind: Pushable.Kind = implicitly[Pushable[T]].kind(value)
+case class Value[T: BytecodeExpression](value: T) {
+  def push(writer: MethodVisitor): Unit = implicitly[BytecodeExpression[T]].push(writer, value)
+  def kind: BytecodeExpression.Kind = implicitly[BytecodeExpression[T]].kind(value)
 }
--- src/main/scala/com/tabishev/leshy/compiler/BytecodeWriter.scala
+++ src/main/scala/com/tabishev/leshy/compiler/BytecodeWriter.scala
@@ -1,15 +1,15 @@
 package com.tabishev.leshy.compiler
 
 import com.tabishev.leshy.bytecode._
-import com.tabishev.leshy.bytecode.intPushable
-import com.tabishev.leshy.bytecode.paramPushable
-import com.tabishev.leshy.bytecode.invokeMethodPushable
+import com.tabishev.leshy.bytecode.intBytecodeExpression
+import com.tabishev.leshy.bytecode.paramBytecodeExpression
+import com.tabishev.leshy.bytecode.invokeMethodBytecodeExpression
 import com.tabishev.leshy.runtime.{FrameOffset, MemoryRef, Runtime, StackMemory}
 import org.objectweb.asm.{MethodVisitor, Opcodes, Type}
 
 import scala.reflect.ClassTag
 
-given memoryOperandPushable: Pushable[MemoryOperand] = Pushable.from {
+given memoryOperandPushable: BytecodeExpression[MemoryOperand] = BytecodeExpression.from {
   case MemoryOperand.Stack(offset) =>
     InvokeMethod.virtual(classOf[StackMemory], "getRef", ContextStack(), offset)
   case MemoryOperand.Native(offset) =>
@@ -19,9 +19,9 @@ given memoryOperandPushable: Pushable[MemoryOperand] = Pushable.from {
 case class ContextStack()
 case class ContextRuntime()
 
-given runtimePushable: Pushable[ContextRuntime] =
-  Pushable.from(Param.idx[Runtime](0))
-given frameOffsetPushable: Pushable[FrameOffset] =
-  Pushable.from(value => InvokeMethod.static(classOf[FrameOffset], "nonNegative", value.get))
-given stackPushable: Pushable[ContextStack] =
-  Pushable.from(InvokeMethod.virtual(classOf[Runtime], "stack", ContextRuntime()))
+given runtimePushable: BytecodeExpression[ContextRuntime] =
+  BytecodeExpression.from(Param.idx[Runtime](0))
+given frameOffsetPushable: BytecodeExpression[FrameOffset] =
+  BytecodeExpression.from(value => InvokeMethod.static(classOf[FrameOffset], "nonNegative", value.get))
+given stackPushable: BytecodeExpression[ContextStack] =
+  BytecodeExpression.from(InvokeMethod.virtual(classOf[Runtime], "stack", ContextRuntime()))
