b44bae0660008b4a5cc0b22e663563d6144aec46
Timur Abishev <abishev.timur@gmail.com>
2021-11-15 11:23:29 -0500

    add generating impls

--- src/main/scala/com/tabishev/leshy/examples/Implementations.scala
+++ src/main/scala/com/tabishev/leshy/examples/Implementations.scala
@@ -12,8 +12,10 @@ object Implementations {
     "interpreter/no-check" -> interpreterNoCheck.run(FnSpecs.Fib4),
     "interpreter/generic" -> interpreter.run(FnSpecs.Fibx4),
     "interpreter/generic/no-check" -> interpreterNoCheck.run(FnSpecs.Fibx4),
-    "compiler" -> FnSpecs.createCompiler(FnSpecs.Fib4),
-    "compiler/generic" -> FnSpecs.createCompiler(FnSpecs.Fibx4),
+    "compiler/no-gen" -> FnSpecs.createCompiler(FnSpecs.Fib4, doBytecodeGeneration = false),
+    "compiler/gen" -> FnSpecs.createCompiler(FnSpecs.Fib4, doBytecodeGeneration = true),
+    "compiler/generic/no-gen" -> FnSpecs.createCompiler(FnSpecs.Fibx4, doBytecodeGeneration = false),
+    "compiler/generic/gen" -> FnSpecs.createCompiler(FnSpecs.Fibx4, doBytecodeGeneration = true),
     "native" -> (n => JavaImpl.fib4(n)),
   )
 
@@ -22,15 +24,18 @@ object Implementations {
     "interpreter/no-check" -> interpreterNoCheck.run(FnSpecs.Fib8),
     "interpreter/generic" ->interpreter.run(FnSpecs.Fibx8),
     "interpreter/generic/no-check" ->interpreterNoCheck.run(FnSpecs.Fibx8),
-    "compiler" -> FnSpecs.createCompiler(FnSpecs.Fib8),
-    "compiler/generic" -> FnSpecs.createCompiler(FnSpecs.Fibx8),
+    "compiler/no-gen" -> FnSpecs.createCompiler(FnSpecs.Fib8, doBytecodeGeneration = false),
+    "compiler/gen" -> FnSpecs.createCompiler(FnSpecs.Fib8, doBytecodeGeneration = true),
+    "compiler/generic/no-gen" -> FnSpecs.createCompiler(FnSpecs.Fibx8, doBytecodeGeneration = false),
+    "compiler/generic/gen" -> FnSpecs.createCompiler(FnSpecs.Fibx8, doBytecodeGeneration = true),
     "native" -> (n => JavaImpl.fib8(n)),
   )
 
   val Fact4: Map[String, Int => Int] = Map(
     "interpreter/generic" -> interpreter.run(FnSpecs.Ffactorial4),
     "interpreter/generic/no-check" -> interpreterNoCheck.run(FnSpecs.Ffactorial4),
-    "compiler/generic" -> FnSpecs.createCompiler(FnSpecs.Ffactorial4),
+    "compiler/generic/no-gen" -> FnSpecs.createCompiler(FnSpecs.Ffactorial4, doBytecodeGeneration = false),
+//    "compiler/generic/gen" -> FnSpecs.createCompiler(FnSpecs.Ffactorial4, doBytecodeGeneration = true),
     "native" -> (n => JavaImpl.ffactorial4(n)),
     "byte buffer" -> (n => ByteBufferImpl.ffactorial4(n)),
     "memory access" -> (n => MemoryAccessImpl.ffactorial4(n)),
@@ -39,7 +44,8 @@ object Implementations {
   val Fact8: Map[String, Int => Long] = Map(
     "interpreter/generic" ->interpreter.run(FnSpecs.Ffactorial8),
     "interpreter/generic/no-check" ->interpreterNoCheck.run(FnSpecs.Ffactorial8),
-    "compiler/generic" -> FnSpecs.createCompiler(FnSpecs.Ffactorial8),
+    "compiler/generic/no-gen" -> FnSpecs.createCompiler(FnSpecs.Ffactorial8, doBytecodeGeneration = false),
+//    "compiler/generic/gen" -> FnSpecs.createCompiler(FnSpecs.Ffactorial8, doBytecodeGeneration = true),
     "native" -> (n => JavaImpl.ffactorial8(n)),
     "byte buffer" -> (n => ByteBufferImpl.ffactorial8(n)),
     "memory access" -> (n => MemoryAccessImpl.ffactorial8(n)),
