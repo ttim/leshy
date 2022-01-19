96bdb06c929088565b30e60f0052eb41b7336652
Timur Abishev <abishev.timur@gmail.com>
2022-01-18 22:08:21 -0500

    simplify interpreter engine

--- leshy-rust/docs/perf.md
+++ leshy-rust/docs/perf.md
@@ -4,7 +4,7 @@ cargo test webasm::fib_tests::test_specialized_interpreter_eval --release
 finished in 1.05s
 
 cargo test webasm::fib_tests::test_caching_interpreter_eval --release
-finished in 2.07
+finished in 2.44
 
 cargo test webasm::fib_tests::test_cached_node_eval --release
 finished in 10.63s
--- leshy-rust/src/core/driver/interpreter.rs
+++ leshy-rust/src/core/driver/interpreter.rs
@@ -19,59 +19,40 @@ impl Interpreter {
 
     // returns true - suspended on unknown node, false - otherwise
     pub fn run(&self, state: &mut RunState, stack: &mut [u8]) -> bool {
-        let frame = state.frames.pop().unwrap();
-        match self.run_internal(frame.id, &mut stack[frame.offset..]) {
-            None => { false }
-            Some(mut suspended_in) => {
-                suspended_in.reverse();
-                suspended_in.iter_mut().for_each(|suspended_frame| suspended_frame.offset += frame.offset);
-                state.frames.append(&mut suspended_in);
-                true
-            }
-        }
-    }
+        while !state.frames.is_empty() {
+            let current = state.frames.pop().unwrap();
 
-    fn run_internal(&self, node: NodeId, stack: &mut [u8]) -> Option<Vec<Frame>> {
-        let mut current = node;
-        loop {
-            match self.get(current) {
+            match self.get(current.id) {
                 None => {
-                    return Some(vec![Frame { id: current, offset: 0 }]);
+                    state.frames.push(current);
+                    return true;
                 }
                 Some(kind) => {
                     match kind {
                         NodeKind::Command { command, next } => {
-                            eval_command(command, stack);
-                            current = *next;
+                            eval_command(command, &mut stack[current.offset..]);
+                            state.frames.push(Frame { id: *next, offset: current.offset } );
                         }
                         NodeKind::Branch { condition, if_true, if_false } => {
-                            if eval_condition(condition, stack) {
-                                current = *if_true;
+                            if eval_condition(condition, &mut stack[current.offset..]) {
+                                state.frames.push(Frame { id: *if_true, offset: current.offset });
                             } else {
-                                current = *if_false;
+                                state.frames.push(Frame { id: *if_false, offset: current.offset });
                             }
                         }
                         NodeKind::Call { offset, call, next } => {
-                            let offset = *offset as usize;
-                            match self.run_internal(*call, &mut stack[offset..]) {
-                                None => {
-                                    current = *next;
-                                }
-                                Some(mut suspended_trace) => {
-                                    // suspended in sub call
-                                    suspended_trace.iter_mut().for_each(|e| e.offset += offset);
-                                    suspended_trace.push(Frame { id: *next, offset: 0 });
-                                    return Some(suspended_trace);
-                                }
-                            }
+                            state.frames.push(Frame { id: *next, offset: current.offset });
+                            state.frames.push(Frame { id: *call, offset: current.offset + (*offset as usize) })
                         }
                         NodeKind::Final => {
-                            return None;
+                            continue
                         }
                     }
                 }
             }
         }
+
+        false
     }
 
     fn get(&self, id: NodeId) -> Option<&NodeKind<NodeId>> {
